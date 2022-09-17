d1a2222a482494a5af7dfb08b2e12b8140c29af0
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-17 03:10:45 +0300

    init commit

---
+++ exercise.js
@@ -0,0 +1,7 @@
+// Create a variable called primitive and assign it a value. Check if the value is a numeric, string or boolean type and print it in the console.
+
+let primitive = 1;
+
+typeof primitive === "number" ? console.log("primitive is a Number") : 0;
+typeof primitive === "string" ? console.log("primitive is a String") : 0;
+typeof primitive === "boolean" ? console.log("primitive is a Boolean") : 0;
